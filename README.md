4549cf2010c9d85877e6360207841fa1b8b5bad0
Wee Jian Hui Kevin <kevin@Wees-MacBook-Air.local>
2023-11-27 12:30:42 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+<div align="center">
+    <a href="https://chronark.com"><h1 align="center">codebykevin.dev</h1></a>
+
+My personal website, built with [Next.js](https://nextjs.org/), [Tailwind CSS](https://tailwindcss.com/), [Contentlayer](https://www.contentlayer.dev/) and deployed to [Vercel](https://vercel.com/).
+
+</div>
+
+<br/>
+
+## Acknowledgments
+
+I would like to extend my gratitude to Andreas Thomas for their incredible work on this website template. This project is based upon and extends their original work, which was generously shared under the MIT License. The original project has been a valuable foundation for codebykevin.dev, and it has greatly inspired my modifications and additions.
+
+You can find the original work on [GitHub](https://github.com/chronark/chronark.com), and it is licensed under the MIT License, a copy of which is included in this repository.
+
+Their contributions to the open-source community have been invaluable and I highly recommend checking out their other projects and supporting their work.
---
+++ app/layout.tsx
@@ -0,0 +1,76 @@
+import "../global.css";
+import { Inter } from "@next/font/google";
+import LocalFont from "@next/font/local";
+import { Metadata } from "next";
+import { Analytics } from "./components/analytics";
+
+export const metadata: Metadata = {
+  title: {
+    default: "codebykevin.dev",
+    template: "%s | codebykevin.dev",
+  },
+  description: "Where Web Dev Meets Biz Dev",
+  generator: 'Next.js',
+  keywords: ['Next.js', 'React', 'TypeScript', 'JavaScript'],
+  authors: [{ name: 'Kevin', url: 'https://github.com/kevinweejh' }]
+  creator: 'Kevin Wee',
+  openGraph: {
+    title: "codebykevin.dev",
+    description:
+      "Where Web Dev Meets Biz Dev",
+    url: "https://codebykevin.dev",
+    siteName: "codebykevin.dev",
+    images: [
+      {
+        url: "https://codebykevin.dev/og.png",
+        width: 1920,
+        height: 1080,
+      },
+    ],
+    locale: "en-SG",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  icons: {
+    shortcut: "/favicon.png",
+  },
+};
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const calSans = LocalFont({
+  src: "../public/fonts/CalSans-SemiBold.ttf",
+  variable: "--font-calsans",
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en" className={[inter.variable, calSans.variable].join(" ")}>
+      <head>
+        <Analytics />
+      </head>
+      <body
+        className={`bg-black ${process.env.NODE_ENV === "development" ? "debug-screens" : undefined
+          }`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
